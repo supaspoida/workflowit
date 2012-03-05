970e446105a8ab6caa3abb4284b8b988b6075831
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-03-04 21:52:28 -0500

    rails new workflowit -O -T

--- Gemfile
+++ Gemfile
@@ -1,3 +1,11 @@
 source :rubygems
 
 gem 'rails', '3.2.2'
+
+gem 'jquery-rails'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workflowit::Application.config.secret_token = '12ae5ba30ed4376b29a3206babc0e2d8a7d7e4f2b295d608df592734624dc48cb6b5753e2d2855b2e77c3bd215234a737fa980ad7b2666c34597670221a754f3'
